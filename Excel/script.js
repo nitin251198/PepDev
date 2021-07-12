ccb926e15f235bf7dc7c05064103ee9f924e4299
nitin251198 <bhnit625@gmail.com>
2021-07-12 20:05:13 +0530

    up

--- Excel/script.js
+++ Excel/script.js
@@ -1,11 +1,17 @@
 let rowNumberSection = document.querySelector(".row-number-section");
 
-
 let formulaBarSelectedCellArea = document.querySelector(".selected-cell-div");
 
+let cellSection = document.querySelector(".cell-section");
+let columnTagsSection = document.querySelector(".column-tag-section");
 
 let lastCell;
 
+cellSection.addEventListener("scroll", function (e) {
+  rowNumberSection.style.transform = `translateY(-${e.currentTarget.scrollTop}px)`;
+
+  columnTagsSection.style.transform = `translateX(-${e.currentTarget.scrollLeft}px)`;
+});
 
 for (let i = 1; i <= 100; i++) {
   let div = document.createElement("div");
@@ -13,8 +19,6 @@ for (let i = 1; i <= 100; i++) {
   div.classList.add("row-number");
   rowNumberSection.append(div);
 }
-
-let columnTagsSection = document.querySelector(".column-tag-section");
 
 for (let i = 0; i < 26; i++) {
   let asciiCode = 65 + i;
@@ -27,19 +31,17 @@ for (let i = 0; i < 26; i++) {
   columnTagsSection.append(div);
 }
 
-
-let cellSection = document.querySelector(".cell-section")
-
 for (let i = 1; i <= 100; i++) {
   let rowDiv = document.createElement("div");
   rowDiv.classList.add("row");
-                       //i = 1 [A1,B1..........Z1]
-                       //i = 2 []
-                       //.
-                       //.
-                       //i = 100 [A100.........z100]
+  //i = 1 [A1,B1..........Z1]
+  //i = 2 []
+  //.
+  //.
+  //i = 100 [A100.........z100]
 
-  for (let j = 0; j < 26; j++) {       //i = 100   j = 25  asciiCode = 65+25=90  alpha = z  cellAdd = Z100
+  for (let j = 0; j < 26; j++) {
+    //i = 100   j = 25  asciiCode = 65+25=90  alpha = z  cellAdd = Z100
     // A to Z
     let asciiCode = 65 + j;
 
@@ -49,34 +51,30 @@ for (let i = 1; i <= 100; i++) {
 
     let cellDiv = document.createElement("div");
 
-     // cellDiv.contentEditable = true;
+    // cellDiv.contentEditable = true
 
     cellDiv.setAttribute("contentEditable", true);
 
     cellDiv.classList.add("cell");
 
-    // cellDiv.setAttribute("data-address", cellAddress);
-
     cellDiv.setAttribute("data-address", cellAddress);
-    
-    cellDiv.addEventListener("click", function(e){
-      if(lastCell){
-        lastCell.classList.remove("cell-selected");
 
+    cellDiv.addEventListener("click", function (e) {
+      if (lastCell) {
+        lastCell.classList.remove("cell-selected");
       }
 
       e.currentTarget.classList.add("cell-selected");
 
-      lastCell= e.currentTarget;
+      lastCell = e.currentTarget;
 
       let currCellAddress = e.currentTarget.getAttribute("data-address");
 
       formulaBarSelectedCellArea.innerText = currCellAddress;
-    })
+    });
 
     rowDiv.append(cellDiv);
   }
 
-  cellSection.append(rowDiv)
-
+  cellSection.append(rowDiv);
 }
