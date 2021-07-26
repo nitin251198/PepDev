bc37f0489bca2eeb91a1dd47665ae8d9151f5dad
nitin251198 <bhnit625@gmail.com>
2021-07-26 21:32:50 +0530

    up

--- Camera/script.js
+++ Camera/script.js
@@ -1,80 +1,111 @@
 let videoPlayer = document.querySelector("video");
 let recordBtn = document.querySelector("#record");
 let captureBtn = document.querySelector("#capture");
+let body = document.querySelector("body");
+let mediaRecorder;
+let chunks = [];
+let isRecording = false;
+let filter = "";
 
+let allFilters = document.querySelectorAll(".filter");
 
-      let mediaRecorder;
-      let chunks = [];
-      let isRecording = false;
+for (let i = 0; i < allFilters.length; i++) {
+  allFilters[i].addEventListener("click", function (e) {
+    let previousFilter = document.querySelector(".filter-div");
 
-      captureBtn.addEventListener("click", function(){
-          let innerSpan = captureBtn.querySelector("span");
+    if (previousFilter) previousFilter.remove();
 
-          innerSpan.classList.add("capture-animation");
-          setTimeout(function()  {
-              innerSpan.classList.remove("capture-animation");
-          }, 1000);
-          let canvas = document.createElement("canvas");
-          canvas.width = videoPlayer.videoWidth;
-          canvas.height = videoPlayer.videoHeight;
+    let color = e.currentTarget.style.backgroundColor;
+    filter = color;
+    let div = document.createElement("div");
+    div.classList.add("filter-div");
+    div.style.backgroundColor = color;
+    body.append(div);
+  });
+}
 
-          let tool = canvas.getContext("2d");
+captureBtn.addEventListener("click", function () {
+  let innerSpan = captureBtn.querySelector("span");
 
-          tool.drawImage(videoPlayer,0,0);
+  innerSpan.classList.add("capture-animation");
+  setTimeout(function () {
+    innerSpan.classList.remove("capture-animation");
+  }, 1000);
 
-          let url = canvas.toDataURL();
+  let canvas = document.createElement("canvas");
+  canvas.width = videoPlayer.videoWidth; //1280x720
+  canvas.height = videoPlayer.videoHeight;
 
-          let a = document.createElement("a");
-          a.href = url;
-          a.download = "image.png";
-          a.click();
-          a.remove();
-      })
+  let tool = canvas.getContext("2d");
 
-      recordBtn.addEventListener("click", function () {
-        let innerSpan = recordBtn.querySelector("span");
-        if(isRecording){
-            //recording ko stop krna h
-            mediaRecorder.stop()
-            isRecording = false
-            innerSpan.classList.remove("record-animation");
-        }else{
-            //recording shuru krni hai 
-            mediaRecorder.start()
-            isRecording = true
-            innerSpan.classList.add("record-animation");
-        }
-      });
+  tool.drawImage(videoPlayer, 0, 0);
 
-      let promiseToUseCamera = navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: true,
-      });
+  if (filter != "") {
+    tool.fillStyle = filter;
+    tool.fillRect(0, 0, canvas.width, canvas.height);
+  }
 
-      promiseToUseCamera
-        .then(function (mediaStream) {
-          // lamen terms me mediaStream ek object hai jisme continously camera and mic ka input ara hai and wo input fir maine using objects video me dalra hu
-          videoPlayer.srcObject = mediaStream;
+  let url = canvas.toDataURL();
+  canvas.remove();
 
-          mediaRecorder = new MediaRecorder(mediaStream);
+  let a = document.createElement("a");
+  a.href = url;
+  a.download = "image.png";
+  a.click();
+  a.remove();
+});
 
-          mediaRecorder.addEventListener("dataavailable", function (e) {
-            chunks.push(e.data);
-          });
+recordBtn.addEventListener("click", function () {
+  let innerSpan = recordBtn.querySelector("span");
 
-          mediaRecorder.addEventListener("stop", function (e) {
-            let blob = new Blob(chunks, { type: "video/mp4" });
-            chunks = [];
+  let previousFilter = document.querySelector(".filter-div");
 
-            let link = URL.createObjectURL(blob); //kisi tarike se blob ki link bnadi h
+  if (previousFilter) previousFilter.remove();
 
-            let a = document.createElement("a");
-            a.href = link;
-            a.download = "video.mp4";
-            a.click();
-            a.remove();
-          });
-        })
-        .catch(function () {
-          console.log("user has denied the access of camera");
-        });
+  filter = "";
+
+  if (isRecording) {
+    //recording ko stop krna h
+    mediaRecorder.stop();
+    isRecording = false;
+    innerSpan.classList.remove("record-animation");
+  } else {
+    //recording shuru krni hai
+    mediaRecorder.start();
+    isRecording = true;
+    innerSpan.classList.add("record-animation");
+  }
+});
+
+let promiseToUseCamera = navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true,
+});
+
+promiseToUseCamera
+  .then(function (mediaStream) {
+    // lamen terms me mediaStream ek object hai jisme continously camera and mic ka input ara hai and wo input fir maine using objects video me dalra hu
+    videoPlayer.srcObject = mediaStream;
+
+    mediaRecorder = new MediaRecorder(mediaStream);
+
+    mediaRecorder.addEventListener("dataavailable", function (e) {
+      chunks.push(e.data);
+    });
+
+    mediaRecorder.addEventListener("stop", function (e) {
+      let blob = new Blob(chunks, { type: "video/mp4" });
+      chunks = [];
+
+      let link = URL.createObjectURL(blob); //kisi tarike se blob ki link bnadi h
+
+      let a = document.createElement("a");
+      a.href = link;
+      a.download = "video.mp4";
+      a.click();
+      a.remove();
+    });
+  })
+  .catch(function () {
+    console.log("user has denied the access of camera");
+  });
