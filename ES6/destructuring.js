9747e56ab98d51c87966ff8e47ac3f84e7b1e11c
nitin251198 <bhnit625@gmail.com>
2021-08-05 16:21:09 +0530

    up

---
+++ ES6/destructuring.js
@@ -0,0 +1,11 @@
+let address = {
+                city: "Delhi",
+                region: "Sonia Vihar",
+                pin: 110094
+}
+
+let {city, region , pin} = address
+
+console.log(city);
+console.log(region);
+console.log(pin);
