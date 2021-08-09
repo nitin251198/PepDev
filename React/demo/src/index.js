375e5a5050d251e52838c71eae7733bd22ea0188
nitin251198 <bhnit625@gmail.com>
2021-08-09 20:25:58 +0530

    up

--- React/demo/src/index.js
+++ React/demo/src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// Libraries
+import React from 'react'; // creation logic
+
+import ReactDOM from 'react-dom'; // render logic or display
+
+// App is a component we have imported
 import App from './App';
 
 ReactDOM.render(
   
-    <App /> ,
-  document.getElementById('root')
+    <App /> ,  // ek component
+  document.getElementById('root') // jagah jha us component ko us ekrte h
 );
 
